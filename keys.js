5c218a1931eb69ed57e529a4b9b5f5beb75020e7
VAHORA <rafique_vahora@agilent.com>
2017-10-03 19:51:48 -0400

    followed homework_instructions readme

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'CQNAM6lX0JULbo3mB73QYjTOz',
+  consumer_secret: 'FpO1ep5Pni2c7nYYgmGKnz7rHcxBd1cD9rN2Fl4UCOh5oOJtAG',
+  access_token_key: '594045683-zDyaWRRBG8PH75WfOTudZbFGIQJeifu2nKLBRkhg',
+  access_token_secret: 'UadvNO29icQdkBp8dF0C2pZpvfRMiJGeHBW56NvUUF1LD',
+}
+
+module.exports = twitterKeys;
+
