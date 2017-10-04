cfdcb4858fb4a2691be5fad0896f47db92fef987
VAHORA <rafique_vahora@agilent.com>
2017-10-03 20:03:42 -0400

    installed packages and set up spotify

--- liri.js
+++ liri.js
@@ -1,1 +1,20 @@
-liri.js
+//Grab data from key.js
+
+var keys = require("./keys.js");
+
+//Take commands
+var input = process.argv[2];
+var action = process.argv[3];
+var nodeArgs = process.argv;
+
+//Spotify
+
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+  id: "67255e6d4ca14f8f9e44a5d9513f2a25",
+  secret: "1a9eb7e3fee647cea3666cc2054eb3ec",
+});
+
+var songName = "";
+
